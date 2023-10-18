c7d4bd1b075788fcc224b977c4985dd641d7f5a0
Peter Kure <peterkure256@gmail.com>
2023-10-18 15:52:39 +0300

    Navigation

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/Login';
 import SignupScreen from './screens/Signup';
 import HomeScreen from './screens/Home';
+import ProfileScreen from './screens/Profile';
+import FeedScreen from './screens/Feed';
+import EditProfileScreen from './screens/EditProfile';
 
 
 const Stack = createNativeStackNavigator();
@@ -27,8 +30,23 @@ function App() {
           name="Home"
           component={HomeScreen} // Add the HomeScreen as a screen
           options={{ title: 'Home' }} // Customize the title as needed
+        
         />
-        {/* Add more screens as needed */}
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen} // Add the HomeScreen as a screen
+          options={{ title: 'Profile' }} // Customize the title as needed
+        />
+        <Stack.Screen
+          name="Feed"
+          component={FeedScreen} // Add the HomeScreen as a screen
+          options={{ title: 'Feed' }} // Customize the title as needed
+        />
+        <Stack.Screen
+          name="Edit Profile"
+          component={EditProfileScreen} // Add the HomeScreen as a screen
+          options={{ title: 'Edit Profile' }} // Customize the title as needed
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
